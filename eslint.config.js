022005c66986a29ef7fb335d3da4f894a2a36d4b
Yoon-seok,Kang <daengdo@saerom.co.kr>
2025-01-17 16:52:21 +0900

    chore: support styleX env

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import tseslint from "typescript-eslint";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import styleXPlugin from "@stylexjs/eslint-plugin";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -18,11 +19,13 @@ export default tseslint.config(
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "jsx-a11y": jsxA11y,
+      "@stylexjs": styleXPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       ...jsxA11y.configs.recommended.rules,
+      ...styleXPlugin.rules,
     },
   },
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import styleX from "vite-plugin-stylex";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), styleX()],
   base: "/project-gd/",
 });
